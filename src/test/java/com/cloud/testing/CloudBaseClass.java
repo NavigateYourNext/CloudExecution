ba68501b6164fdce9d6b7ef9afe37c537c45f133
Akshay Shete <akshay.shete@ad.infosys.com>
2020-09-30 00:55:14 +0530

    BrowserStack & SauceLabs Execution

--- src/test/java/com/cloud/testing/CloudBaseClass.java
+++ src/test/java/com/cloud/testing/CloudBaseClass.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -25,11 +26,11 @@ public class CloudBaseClass
 	public static ExtentReports extentReports;
 	public static ExtentTest extentTest;
 	public static String selectedCloud="";
-	
+
 	public static final String browserStackUSERNAME = "akshay395";
 	public static final String browserStackAUTOMATE_KEY = "3xNo3pgea85nsRbBxaWa";
 	public static final String browserStackURL = "https://" + browserStackUSERNAME + ":" + browserStackAUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
-	
+
 
 	static
 	{
@@ -61,25 +62,25 @@ public class CloudBaseClass
 
 	public WebDriver setupBrowserStackProperties()
 	{
-		
+
 		try
 		{
 			prop = new Properties();
 			FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/browserStack.properties");
 			prop.load(fis);
-			
+
 			String os = prop.getProperty("os");
 			String os_version = prop.getProperty("os_version");
 			String browser = prop.getProperty("browser");
 			String browser_version = prop.getProperty("browser_version");
 			String name = prop.getProperty("name");
-			
+
 			DesiredCapabilities caps = new DesiredCapabilities();
 			caps.setCapability("os", os);
 			caps.setCapability("os_version", os_version);
 			caps.setCapability("browser_version", browser_version);
 			caps.setCapability("name", name);
-			
+
 			if(browser.equalsIgnoreCase("Chrome"))
 			{
 				WebDriverManager.chromedriver().setup();
@@ -90,7 +91,7 @@ public class CloudBaseClass
 				WebDriverManager.firefoxdriver().setup();
 				caps.setCapability("browser", browser);
 			}
-			
+
 			driver = new RemoteWebDriver(new URL(browserStackURL),caps);
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
@@ -99,12 +100,60 @@ public class CloudBaseClass
 		{
 			e.printStackTrace();
 		}
-		
+
 		return driver;
 	}
 
 	public WebDriver setupSauceLabsProperties()
 	{
+		try
+		{
+			prop = new Properties();
+			FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/sauceLabs.properties");
+			prop.load(fis);
+
+			String build = prop.getProperty("build");
+			String seleniumVersion = prop.getProperty("seleniumVersion");
+			String username = prop.getProperty("username");
+			String accessKey = prop.getProperty("accessKey");
+			String tags = prop.getProperty("tags");
+			String browser = prop.getProperty("browser");
+
+			
+			MutableCapabilities sauceOptions = new MutableCapabilities();
+			sauceOptions.setCapability("build", "Java-W3C-Examples");
+			sauceOptions.setCapability("seleniumVersion", "3.141.59");
+			sauceOptions.setCapability("username", "akshayisok");
+			sauceOptions.setCapability("accessKey", "e297d14c-3c21-41cf-8f01-e4b4172deaf1");
+			sauceOptions.setCapability("tags", "w3c-chrome-tests");
+
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setCapability("sauce:options", sauceOptions);
+			caps.setCapability("browserVersion", "latest");
+			caps.setCapability("platformName", "windows 10");
+
+			if(browser.equalsIgnoreCase("Chrome"))
+			{
+				WebDriverManager.chromedriver().setup();
+				caps.setCapability("browserName", browser);
+			}
+			else if(browser.equalsIgnoreCase("Firefox"))
+			{
+				WebDriverManager.firefoxdriver().setup();
+				caps.setCapability("browserName", browser);
+			}
+			
+			
+			driver = new RemoteWebDriver(new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub"),caps);
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
 		return driver;
 	}
 }
